0223cf4e04a862bdffc5091a0c232fb2c99d5b92
SoftDevEmma <SoftDevEmma@DESKTOP-I8IPH9N>
2024-04-12 16:46:36 +0100

    Delete Comment

--- api/Interface/ICommentRepository.cs
+++ api/Interface/ICommentRepository.cs
@@ -12,5 +12,6 @@ namespace api.Interface
         Task<Comment?> GetByIdAsync(int id);
         Task<Comment> CreateAsync(Comment commentModel);
         Task<Comment?> UpdateAsync(int id, Comment commentModel);
+        Task<Comment?> DeleteAsync(int id);
     }
 }
