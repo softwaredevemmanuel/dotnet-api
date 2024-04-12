e3dcab34c2cafc01dd31df0d90a55757098e7b43
SoftDevEmma <SoftDevEmma@DESKTOP-I8IPH9N>
2024-04-12 18:17:38 +0100

    Sorting

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -47,10 +47,20 @@ namespace api.Repository
             {
                 stocks = stocks.Where(s => s.CompanyName.Contains(query.CompanyName));
             }
+            
             if(!string.IsNullOrWhiteSpace(query.Symbol))
             {
                 stocks = stocks.Where(s => s.Symbol.Contains(query.Symbol));
             }
+
+            if(!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                if(query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
+                {
+                    stocks = query.IsDecending? stocks.OrderByDescending(s=>s.Symbol) : stocks.OrderBy(s=>s.Symbol);
+                }
+            }
+            
             return await stocks.ToListAsync();
         }
 
