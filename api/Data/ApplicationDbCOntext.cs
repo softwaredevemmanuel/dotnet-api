238c9b060f028b65a59883440ae5ef490acb72d2
SoftDevEmma <SoftDevEmma@DESKTOP-I8IPH9N>
2024-04-12 21:54:37 +0100

    installing and setting up Identity

--- api/Data/ApplicationDbCOntext.cs
+++ api/Data/ApplicationDbCOntext.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using api.Models;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 namespace api.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         public ApplicationDbContext(DbContextOptions dbContextOptions):base(dbContextOptions)
         {
