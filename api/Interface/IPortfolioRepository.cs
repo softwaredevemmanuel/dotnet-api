0603414edc6fcde1c7bef222b3e6c7c255edfd10
SoftDevEmma <SoftDevEmma@DESKTOP-I8IPH9N>
2024-04-14 23:03:02 +0100

    Delete Portfoilio

--- api/Interface/IPortfolioRepository.cs
+++ api/Interface/IPortfolioRepository.cs
@@ -10,5 +10,6 @@ namespace api.Interface
     {
         Task<List<Stock>> GetUserPortfolio(AppUser user);
         Task<Portfolio> CreateAsync(Portfolio portfolio);
+        Task<Portfolio> DeletePortfolio(AppUser appUser, string symbol);
     }
 }
